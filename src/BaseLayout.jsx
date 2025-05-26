29724e5a7f3eac41a22504567687aa2070da57a4
Al Azad Heera <alazadheera@gmail.com>
2025-05-26 13:59:45 +0600

    nProgress and sweet alert 2 added

--- src/BaseLayout.jsx
+++ src/BaseLayout.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import { Outlet } from 'react-router'
+import React, {useEffect} from "react";
+import NavBar from "./components/NavBar";
+import { Outlet } from "react-router";
+import { useNavigation } from "react-router";
 
 function BaseLayout() {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
+
+  useEffect(() => {
+    if (navigation.state === "loading") {
+      nProgress.start();
+    } else {
+      nProgress.done();
+    }
+  }, [navigation.state]);
+
   return (
     <div>
-        <NavBar />
-        <Outlet />
+      <NavBar />
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default BaseLayout
+export default BaseLayout;
